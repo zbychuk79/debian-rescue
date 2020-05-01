5232a2914e016ee91f31ee954fc429825e63b48b
Zbigniew Kurzyca <zbychuk@gmail.com>
2020-05-01 12:00:30 +0200

    pią, 1 maj 2020, 12:00:30 CEST

--- install_script.sh
+++ install_script.sh
@@ -13,24 +13,25 @@ apt install restic rsync fsarchiver dar git -y
 apt clean
 pip3 install telegram-send
 
-
 cd /root
 git clone https://github.com/zbychuk79/debian-rescue.git
-
+cd debian-rescue
+mv czytaj.txt /
+mv install_script.sh /
+mv scripts/restic-script /usr/local/bin
+mv scripts/sysupdate /usr/local/bin
 
 mkdir -p /root/.config
 cd /root/.config
-
-
+git clone https://github.com/zbychuk79/mc.git
 
 cd /root
 git clone https://github.com/zbychuk79/aptitude.git
 mv aptitude/.aptitude .
 #rm -rf aptitude
 
-
 cd /usr/local/bin
-chmod +x *
+#chmod +x *
 echo "rescue" > /etc/hostname
 rm -rf ~/.cache
 #rm /install_script.sh
